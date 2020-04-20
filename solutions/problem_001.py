7a6d3ad2b1c2d0787a43e54b67c8580f21bed599
Saurabh Kadu <2412skadu@gmail.com>
2020-04-20 10:05:25 +0530

    Update problem_001.py

--- solutions/problem_001.py
+++ solutions/problem_001.py
@@ -4,10 +4,11 @@ def check_sums(array, k):
         if num in potential_solutions:
             return True
         potential_solutions.add(k - num)
-    
+
     return False
 
 
 assert not check_sums([], 17)
 assert check_sums([10, 15, 3, 7], 17)
 assert not check_sums([10, 15, 3, 4], 17)
+#learning github
